1afc4e5c2325c83df1197cfcb37f9a25eceac4ac
Avtandil Gokadze <a.gokadze@adjarabet.com>
2017-12-20 13:55:17 +0400

    Write a 'hello world' react component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App = () => <h1>Hello</h1>;
+class App extends React.Component{
+	render(){
+		//return <h1>Hello World</h1>
+		return React.createElement('h1', null, 'Hello Jonh')
+	}
+}
+
+//const App = () => <h1>Hey Dude!</h1>
 
 export default App;
