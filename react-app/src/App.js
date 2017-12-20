517fb2f442664f59e5fab0edba0ee80ecdcf7131
Avtandil Gokadze <a.gokadze@adjarabet.com>
2017-12-20 18:32:12 +0400

    Control React Component Updates When New Props are Received

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,53 +5,40 @@ class App extends React.Component{
 
 	constructor(){
 		super();
-		this.state = {val: 0}
+		this.state = {increasing: false}
 		this.update = this.update.bind(this)
-		console.log("constructor");
 	}
-	
+
 	update(e){
-		this.setState({
-			val: this.state.val + 1
-		})
-	}
-
-	componentWillMount(){
-		console.log("componentWillMount");
-	}
-
-	render(){
-		console.log("render");
-		return <button onClick={this.update}>{this.state.val}</button>
-	}
-	
-	componentDidMount(){
-		console.log("componentDidMount");
-	}
-	
-	componentWillUnmount(){
-		console.log("componentWillUnmount");
-	}
-}
-
-class Wrapper extends React.Component{
-	mount(){
-		ReactDom.render(<App />, document.getElementById('a'))
-	}
-	
-	unmount(){
-		ReactDom.unmountComponentAtNode(document.getElementById('a'))
-	}
-	
-	render(){
-		return (
-			<div>
-				<button onClick={this.mount.bind(this)}>Mount</button>
-				<button onClick={this.unmount.bind(this)}>Unmount</button>
-				<div id="a"></div>
-			</div>
+		ReactDom.render(
+			<App val={this.props.val + 1} />,
+			document.getElementById('root')
 		)
 	}
+	
+	componentWillReceiveProps(nextProps){
+		console.log(nextProps)
+		this.setState({increasing: nextProps.val > this.props.val})
+	}
+	
+	shouldComponentUpdate(nextProps, nextState){
+		return nextProps.val % 5 === 0;
+	}
+	
+	render(){
+		console.log(this.state.increasing)
+		return (
+			<button onClick={this.update}>
+				{this.props.val}
+			</button>
+		)
+	}
+
+	componentDidUpdate(prevProps, prevState){
+		console.log(`prevProps:`, prevProps);
+	}
 }
 
-export default Wrapper;
+App.defaultProps = {val: 0}
+
+export default App;
