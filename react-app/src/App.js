6611d4ffb3210f90758c58f5bdfc9809fd5ebe18
Avtandil Gokadze <a.gokadze@adjarabet.com>
2017-12-20 17:09:37 +0400

    Use React ref to Get a Reference to Specific Components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,32 +4,41 @@ class App extends React.Component{
 
 	constructor(){
 		super();
-		this.state = {currentEvent: '---'}
-		this.update = this.update.bind(this)
+		this.state = {a: ''}
 	}
 	
 	update(e){
-		this.setState({currentEvent: e.type})
+		this.setState({
+			a: this.a.refs.input.value,
+			b: this.refs.b.value
+		})
+
 	}
 
 	render(){
 		return (
 			<div>
-				<textarea
-						onKeyPress={this.update}
-						onCopy={this.update}
-						onCut={this.update}
-						onPaste={this.update}
-						onFocus={this.update}
-						onBlur={this.update}
-						onDoubleClick={this.update}
-						onTouchStart={this.update}
-						onTouchMove={this.update}
-						onTouchEnd={this.update}
-						cols="30"
-						row="10" />
-				<h1>{this.state.currentEvent}</h1>
+				<Input
+						ref={ component => this.a = component}
+						update={this.update.bind(this)}
+				/> {this.state.a}
+				<hr />
+				<input
+						ref="b"
+						type="text"
+						onChange={this.update.bind(this)}
+				/> {this.state.b}
 			</div>
+		)
+	}
+}
+
+class Input extends React.Component {
+	render(){
+		return (
+			<span>
+				<input ref="input" type="text" onChange={this.props.update}/>
+			</span>
 		)
 	}
 }
