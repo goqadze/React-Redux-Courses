6829111d9c9cde8f1325b24f6106bd21b8890b35
Avtandil Gokadze <a.gokadze@adjarabet.com>
2017-12-22 14:18:14 +0400

    Redux: Writing a Todo List Reducer (Adding a Todo)

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,66 +1,45 @@
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
-const addCounter = (list) => {
-//	list.push(0); // Mutation
-//	return list;
-	return [...list, 0]
+const todos = (state = [], action) => {
+	switch (action.type) {
+		case 'ADD_TODO':
+			return [
+				...state,
+				{
+					id: action.id,
+					text: action.text,
+					completed: false
+				}
+			]
+		default:
+			return state;
+	}
 }
 
-const removeCounter = (list, index) => {
-//	return list.splice(index, 1) // Mutation
-	//return list.slice(0, index).concat(list.slice(index + 1));
-	return [
-		...list.slice(0, index),
-		...list.slice(index + 1)
-	]
-}
+const testAddTodo = () => {
+	const stateBefore = [];
+	const action = {
+		type: 'ADD_TODO',
+		id: 0,
+		text: 'Learn Redux'
+	}
+	const stateAfter = [
+		{
+			id: 0,
+			text: 'Learn Redux',
+			completed: false
+		}
+	];
 
-const incrementCounter = (list, index) => {
-	return [
-		...list.slice(0, index),
-		list[index] + 1,
-		...list.slice(index + 1)
-	]
-}
-
-
-const testAddCounter = () => {
-	const listBefore = [];
-	const listAfter = [0];
-
-	deepFreeze(listBefore);
+	deepFreeze(stateBefore);
+	deepFreeze(action);
 
 	expect(
-		addCounter(listBefore)
-	).toEqual(listAfter);
+		todos(stateBefore, action)	
+	).toEqual(stateAfter);
 }
 
-const testRemoveCounter = () => {
-	const listBefore = [0, 10, 20];
-	const listAfter = [0, 20];
-
-	deepFreeze(listBefore);
-
-	expect(
-		removeCounter(listBefore, 1)
-	).toEqual(listAfter)
-}
-
-const testIncrementCounter = () => {
-	const listBefore = [0, 10, 20];
-	const listAfter = [0, 11, 20];
-
-	deepFreeze(listBefore);
-
-	expect(
-		incrementCounter(listBefore, 1)
-	).toEqual(listAfter)
-}
-
-
-testAddCounter();
-testRemoveCounter();
-testIncrementCounter();
+testAddTodo();
 
 console.log('All tests passed.')
