b933d563999f3be8f6e6fe89f3c6e563a5908c7b
Avtandil Gokadze <a.gokadze@adjarabet.com>
2017-12-22 13:49:04 +0400

    Redux: Avoiding Array Mutations with concat(), slice() and ...spread

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,52 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
 
-const counter = (state = 0, action) => {
-	switch (action.type) {
-		case 'INCREMENT':
-			return state + 1;
-		case 'DECREMENT':
-			return state - 1;
-		default:
-			return state;
-	}
+const addCounter = (list) => {
+//	list.push(0); // Mutation
+//	return list;
+	return [...list, 0]
 }
 
-const Counter = ({
-	value,
-	onIncrement,
-	onDecrement,
-}) => {
-	return (
-		<div>
-			<h1>{value}</h1>
-			<button onClick={onIncrement}>+</button>
-			<button onClick={onDecrement}>-</button>
-		</div>
-	)
+const removeCounter = (list, index) => {
+//	return list.splice(index, 1) // Mutation
+	//return list.slice(0, index).concat(list.slice(index + 1));
+	return [
+		...list.slice(0, index),
+		...list.slice(index + 1)
+	]
 }
 
-const store = createStore(counter);
-
-const render = () => {
-	ReactDOM.render(
-		<Counter
-			value={store.getState()}
-			onIncrement={() => 
-				store.dispatch({
-					type: 'INCREMENT'	
-				})
-			}
-			onDecrement={() => 
-				store.dispatch({
-					type: 'DECREMENT'	
-				})
-			}
-		/>,
-		document.getElementById('root')
-	)
+const incrementCounter = (list, index) => {
+	return [
+		...list.slice(0, index),
+		list[index] + 1,
+		...list.slice(index + 1)
+	]
 }
-store.subscribe(render);
-render();
 
+
+const testAddCounter = () => {
+	const listBefore = [];
+	const listAfter = [0];
+
+	deepFreeze(listBefore);
+
+	expect(
+		addCounter(listBefore)
+	).toEqual(listAfter);
+}
+
+const testRemoveCounter = () => {
+	const listBefore = [0, 10, 20];
+	const listAfter = [0, 20];
+
+	deepFreeze(listBefore);
+
+	expect(
+		removeCounter(listBefore, 1)
+	).toEqual(listAfter)
+}
+
+const testIncrementCounter = () => {
+	const listBefore = [0, 10, 20];
+	const listAfter = [0, 11, 20];
+
+	deepFreeze(listBefore);
+
+	expect(
+		incrementCounter(listBefore, 1)
+	).toEqual(listAfter)
+}
+
+
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+
+console.log('All tests passed.')
