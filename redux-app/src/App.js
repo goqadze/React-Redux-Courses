0f49020e97c0a42f21872deb2dfc1558099a371e
Avtandil Gokadze <a.gokadze@adjarabet.com>
2017-12-22 14:56:18 +0400

    Redux: Writing a Todo List Reducer (Toggling a Todo)

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -12,6 +12,17 @@ const todos = (state = [], action) => {
 					completed: false
 				}
 			]
+		case 'TOGGLE_TODO':
+			return state.map(todo => {
+				if (todo.id !== action.id) {
+					return todo;
+				}
+				
+				return {
+					...todo,
+					completed: !todo.completed
+				}
+			})
 		default:
 			return state;
 	}
@@ -40,6 +51,46 @@ const testAddTodo = () => {
 	).toEqual(stateAfter);
 }
 
-testAddTodo();
+const testToggleTodo = () => {
+	const stateBefore = [
+		{
+			id: 0,
+			text: 'Learn Redux',
+			completed: false
+		},
+		{
+			id: 1,
+			text: 'Go shopping',
+			completed: false
+		}
+	];
 
+	const action = {
+		type: 'TOGGLE_TODO',
+		id: 1
+	}
+
+	const stateAfter = [
+		{
+			id: 0,
+			text: 'Learn Redux',
+			completed: false
+		},
+		{
+			id: 1,
+			text: 'Go shopping',
+			completed: true
+		}
+	]
+
+	deepFreeze(stateBefore);
+	deepFreeze(action);
+
+	expect(
+		todos(stateBefore, action)
+	).toEqual(stateAfter);
+}
+
+testAddTodo();
+testToggleTodo();
 console.log('All tests passed.')
