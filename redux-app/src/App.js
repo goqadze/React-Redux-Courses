86ba9b2ef75993d21b9a783a7c727a77853ae88e
Avtandil Gokadze <a.gokadze@adjarabet.com>
2017-12-22 17:10:26 +0400

    Redux: Implementing combineReducers() from Scratch

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,4 +1,4 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore} from 'redux';
 
 const todo = (state, action) => {
 	switch (action.type) {
@@ -50,23 +50,22 @@ const visibilityFilter = (
 	}
 }
 
+const combineReducers = (reducers) => {
+	return (state = {}, action) => {
+		return Object.keys(reducers).reduce(
+			(nextState, key) => {
+				nextState[key] = reducers[key](state[key], action);
+				return nextState;
+			},
+			{}
+		)
+	}
+}
+
 const todoApp = combineReducers({
 	todos,
 	visibilityFilter,
 })
-
-//const todoApp = (state = {}, action) => {
-//	return {
-//		todos: todos(
-//			state.todos,
-//			action
-//		),
-//		visibilityFilter: visibilityFilter(
-//			state.visibilityFilter,
-//			action
-//		)
-//	}
-//}
 
 const store = createStore(todoApp);
 
