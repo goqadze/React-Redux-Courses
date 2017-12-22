95520908143cf4f198ec1e7fb53eab5f6b776c8a
Avtandil Gokadze <a.gokadze@adjarabet.com>
2017-12-22 12:02:03 +0400

    Redux: Store Methods: getState(), dispatch(), and subscribe()

---
+++ redux-app/src/App.js
@@ -0,0 +1,42 @@
+import {createStore} from 'redux';
+
+const counter = (state = 0, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return state + 1;
+		case 'DECREMENT':
+			return state - 1;
+		default:
+			return state;
+	}
+}
+
+const store = createStore(counter);
+
+const render = () => {
+	document.body.innerText = store.getState();
+}
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+	store.dispatch({type: 'INCREMENT'})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const App = () => {}
+export default App;
