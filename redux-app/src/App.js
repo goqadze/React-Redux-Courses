583420279ec3099bfbd5d3190d4cbec18b0603ad
Avtandil Gokadze <a.gokadze@adjarabet.com>
2017-12-22 18:23:17 +0400

    Redux: React Todo List Example (Adding a Todo)

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,4 +1,6 @@
-import {createStore} from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, combineReducers} from 'redux';
 
 const todo = (state, action) => {
 	switch (action.type) {
@@ -50,18 +52,6 @@ const visibilityFilter = (
 	}
 }
 
-const combineReducers = (reducers) => {
-	return (state = {}, action) => {
-		return Object.keys(reducers).reduce(
-			(nextState, key) => {
-				nextState[key] = reducers[key](state[key], action);
-				return nextState;
-			},
-			{}
-		)
-	}
-}
-
 const todoApp = combineReducers({
 	todos,
 	visibilityFilter,
@@ -69,37 +59,48 @@ const todoApp = combineReducers({
 
 const store = createStore(todoApp);
 
-console.log('Initial state:');
-console.log(store.getState());
-console.log('--------------');
+let nextTodoId = 0;
+class TodoApp extends React.Component {
+	render() {
+		return (
+			<div>
+				<input ref={node => {
+					this.input = node;
+				}} />
+				<button
+					onClick={() => {
+						store.dispatch({
+							type: 'ADD_TODO',
+							text: this.input.value,
+							id: nextTodoId++
+						})
+						this.input.value = '';
+					}}
+				>
+					Add Todo
+				</button>
+				<ul>
+					{this.props.todos.map(todo => 
+						<li key={todo.id}>
+							{todo.text}
+						</li>
+					)}
+				</ul>
+			</div>
+		)
+	}
+}
 
-console.log('Dispatching ADD_TODO.');
-store.dispatch({
-	type: 'ADD_TODO',
-	id: 0,
-	text: 'Learn Redux'
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
 
+const render = () => {
+	ReactDOM.render(
+		<TodoApp 
+			todos={store.getState().todos}
+		/>,
+		document.getElementById('root')
+	);
+}
 
-console.log('Dispatching TOGGLE_TODO.');
-store.dispatch({
-	type: 'TOGGLE_TODO',
-	id: 0,
-	text: 'Learn Redux'
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching SET_VISIBILITY_FILTER');
-store.dispatch({
-	type: 'SET_VISIBILITY_FILTER',
-	filter: 'SHOW_COMPLETED'
-})
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
+store.subscribe(render);
+render();
 
