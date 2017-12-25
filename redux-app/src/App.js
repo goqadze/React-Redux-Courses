9e44712d92a246893e55fb313c6cd6077aee2dc2
Avtandil Gokadze <a.gokadze@adjarabet.com>
2017-12-25 17:15:53 +0400

    REdux: Extracting Container Componenets (VisibleTodoList, AddTodo)

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -160,7 +160,11 @@ const AddTodo = ({
 			}} />
 			<button
 				onClick={() => {
-					onAddClick(input.value);
+					store.dispatch({
+						type: 'ADD_TODO',
+						id: nextTodoId++,
+						text: input.value
+					})
 					input.value = '';
 				}}
 			>
@@ -208,48 +212,53 @@ const getVisibleTodos = (
 	}
 }
 
+class VisibleTodoList extends React.Component {
+	componentDidMount() {
+		this.unsubscribe = store.subscribe(() => 
+			this.forceUpdate()
+		)
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
+	render() {
+		const props = this.props;
+		const state = store.getState();
+
+		return (
+			<TodoList
+				todos={
+					getVisibleTodos(
+						state.todos,
+						state.visibilityFilter
+					)
+				}
+				onTodoClick={id => 
+					store.dispatch({
+						type: 'TOGGLE_TODO',
+						id
+					})
+				}
+			/>
+		)
+	}
+}
+
 let nextTodoId = 0;
-const TodoApp = ({
-	todos,
-	visibilityFilter
-}) => ( 
+const TodoApp = () => ( 
 	<div>
-		<AddTodo 
-			onAddClick={text => 
-				store.dispatch({
-					type: 'ADD_TODO',
-					id: nextTodoId++,
-					text
-				})
-			}
-		/>
-		<TodoList
-			todos={
-				getVisibleTodos(
-					todos,
-					visibilityFilter
-				)}
-			onTodoClick={id => 
-				store.dispatch({
-					type: 'TOGGLE_TODO',
-					id
-				})
-			}
-		/>
+		<AddTodo />
+		<VisibleTodoList />
 		<Footer />
 	</div>
 )
 
 
-const render = () => {
-	ReactDOM.render(
-		<TodoApp 
-			{...store.getState()}
-		/>,
-		document.getElementById('root')
-	);
-}
+ReactDOM.render(
+	<TodoApp />,
+	document.getElementById('root')
+);
 
-store.subscribe(render);
-render();
 
