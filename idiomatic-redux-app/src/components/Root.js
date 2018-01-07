c55adec3f087d4642cab2f09627fe9cf5b9b319a
agokadze <agoqadze@gmail.com>
2018-01-07 20:20:53 +0400

    fix dependencies issues

--- idiomatic-redux-app/src/components/Root.js
+++ idiomatic-redux-app/src/components/Root.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux'; 
 import App from './App';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
 const Root = ({ store }) => (
   <Provider store={store}>
