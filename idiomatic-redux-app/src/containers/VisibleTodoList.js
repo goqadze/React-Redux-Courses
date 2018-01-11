ae24ac7810bc62145218334be35c9a07f2c4d0bb
agokadze <agoqadze@gmail.com>
2018-01-11 16:42:26 +0400

    Redux: Colocate Selectors with Reducers

--- idiomatic-redux-app/src/containers/VisibleTodoList.js
+++ idiomatic-redux-app/src/containers/VisibleTodoList.js
@@ -1,23 +1,11 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { toggleTodo } from '../actions';
+import { getVisibleTodos } from '../reducers';
 import TodoList from '../components/TodoList';
 
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'all':
-      return todos;
-    case 'completed':
-      return todos.filter(t => t.completed);
-    case 'active':
-      return todos.filter(t => !t.completed);
-    default:
-      throw new Error(`Unknown filter ${filter}`);
-  }
-};
-
 const mapStateToProps = (state, { match }) => ({
-  todos: getVisibleTodos(state.todos, match.params.filter || 'all')
+  todos: getVisibleTodos(state, match.params.filter || 'all')
 });
 
 //const mapDispatchToProps = (dispatch) => ({
--- idiomatic-redux-app/src/reducers/index.js
+++ idiomatic-redux-app/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import todos from './todos';
+import todos, * as fromTodos from './todos';
 import visibilityFilter from './visibilityFilter';
 
 const todoApp = combineReducers({
@@ -8,3 +8,6 @@ const todoApp = combineReducers({
 });
 
 export default todoApp;
+
+export const getVisibleTodos = (state, filter) => 
+  fromTodos.getVisibleTodos(state.todos, filter);
