76828e705c0d836854ce594f02d7d295bf313993
agokadze <agoqadze@gmail.com>
2018-01-11 16:46:26 +0400

    Redux: Colocate Selectors with Reducers

--- idiomatic-redux-app/src/reducers/index.js
+++ idiomatic-redux-app/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import todos, * as fromTodos from './todos';
-import visibilityFilter from './visibilityFilter';
 
 const todoApp = combineReducers({
   todos,
-  visibilityFilter
 });
 
 export default todoApp;
