5abbe5c932ddc7bb6ed1895537d0ccaf034578de
agokadze <agoqadze@gmail.com>
2018-01-12 16:11:05 +0400

    Redux: Fetching Data on Route Change

--- idiomatic-redux-app/src/containers/VisibleTodoList.js
+++ idiomatic-redux-app/src/containers/VisibleTodoList.js
@@ -1,22 +1,42 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { toggleTodo } from '../actions';
 import { getVisibleTodos } from '../reducers';
 import TodoList from '../components/TodoList';
+import { fetchTodos } from '../api';
 
-const mapStateToProps = (state, { match }) => ({
-  todos: getVisibleTodos(state, match.params.filter || 'all')
-});
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    fetchTodos(this.props.filter).then(todos => {
+      console.log(this.props.filter, todos);
+    });
+  }
 
-//const mapDispatchToProps = (dispatch) => ({
-//  onTodoClick(id) {
-//    dispatch(toggleTodo(id));
-//  }
-//});
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos => {
+        console.log(this.props.filter, todos);
+      });
+    }
+  }
 
-const VisibleTodoList = withRouter(connect(
+  render() {
+    return <TodoList {...this.props} />
+  }
+} 
+
+const mapStateToProps = (state, { match }) => {
+  const filter = match.params.filter || 'all';
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter,
+  }
+};
+
+VisibleTodoList = withRouter(connect(
   mapStateToProps,
   { onTodoClick: toggleTodo }
-)(TodoList));
+)(VisibleTodoList));
 
 export default VisibleTodoList;
