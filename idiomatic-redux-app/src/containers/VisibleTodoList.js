b49a3ad9f3abb51208464cd8ce2682213c6f9d94
agokadze <agoqadze@gmail.com>
2018-01-13 14:05:41 +0400

    Redux: Displaying Loading Indicators

--- idiomatic-redux-app/src/containers/VisibleTodoList.js
+++ idiomatic-redux-app/src/containers/VisibleTodoList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import * as actions from '../actions';
-import { getVisibleTodos } from '../reducers';
+import { getVisibleTodos, getIsFetching } from '../reducers';
 import TodoList from '../components/TodoList';
 
 class VisibleTodoList extends Component {
@@ -17,15 +17,20 @@ class VisibleTodoList extends Component {
   }
 
   fetchData() {
-    const { filter, fetchTodos } = this.props;
+    const { filter, requestTodos, fetchTodos } = this.props;
+    requestTodos(filter);
     fetchTodos(filter);
   }
 
   render() {
-    const { toggleTodo, ...rest} = this.props;
+    const { toggleTodo, todos, isFetching } = this.props;
+    if(isFetching && !todos.length) {
+      return <p>Loading...</p>
+    }
+    
     return (
       <TodoList 
-        {...rest} 
+        todos={todos}
         onTodoClick={toggleTodo}
       /> 
     )
@@ -36,6 +41,7 @@ const mapStateToProps = (state, { match }) => {
   const filter = match.params.filter || 'all';
   return {
     todos: getVisibleTodos(state, filter),
+    isFetching: getIsFetching(state, filter),
     filter,
   }
 };
