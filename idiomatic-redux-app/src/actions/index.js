a216f118cc8ba57b340089e7f0230fa2cea3f7e9
root <root@a-gogadze.corp.adjarabet.com>
2018-01-16 14:41:51 +0400

    Redux: Normalizing API Responses with normalizr

--- idiomatic-redux-app/src/actions/index.js
+++ idiomatic-redux-app/src/actions/index.js
@@ -1,3 +1,5 @@
+import { normalize } from 'normalizr';
+import * as schema from './schema';
 import { getIsFetching } from '../reducers';
 import * as api from '../api';
 
@@ -13,10 +15,11 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
 
   return api.fetchTodos(filter).then(
     response => {
+      console.log(response);
       dispatch({
         type: 'FETCH_TODOS_SUCCESS',
         filter,
-        response,
+        response: normalize(response, schema.arrayOfTodos)
       });
     },
     error => {
@@ -33,7 +36,7 @@ export const addTodo = (text) => (dispatch) =>
   api.addTodo(text).then(response => {
     dispatch({
       type: 'ADD_TODO_SUCCESS',
-      response,
+      response: normalize(response, schema.todo)
     });
   });
 
